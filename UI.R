fb689c2647f9b3931167e215e7dad8e4717a3ba9
MarkDunning <mark.dunning@gmail.com>
2014-03-31 10:58:56 +0100

    Shiny example

---
+++ UI.R
@@ -0,0 +1,54 @@
+library(shiny)
+library(plotrix)
+
+shinyUI(pageWithSidebar(
+  
+  headerPanel("Regression Example"),
+  
+  sidebarPanel(
+    selectInput("slope","Slope:",
+                list("Flat","Slight","Steeper","Steepest"),"Slight"),
+    selectInput("noise","Noisiness of the data:",
+                list("Low","Medium","High")),
+    sliderInput("n","Multiply the number of observations by:",
+                min = 1,
+                max = 10,
+                value = 1),
+    br(),
+    h4("To do"),
+    helpText("Try changing the slope of the line,
+             the noisiness of the data or the number of observations.
+             You will be able to see the change on the plot. Can you also
+             determine its effect on the Summary Statistics?"),
+    br(),
+    checkboxInput("line","Show regression line?", T),
+    checkboxInput("means","Show mean of X and Y?"),
+    checkboxInput("ant","Show regression equation?", F)
+    ),
+  
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Plot",plotOutput("plot")),
+      tabPanel("Summary Statistics",
+               h4("Screen output in R"),
+               verbatimTextOutput("summary")),
+      tabPanel("Example  R code",
+               helpText("#Generate some data and place in a dataframe"),
+               helpText("X <- 1:100"),
+               helpText("Y <- 100 + 2*X + rnorm(100, sd=10)"),
+               helpText("mydata <- data.frame(X, Y)"),
+               br(),
+               helpText("#Fit regression model"),
+               helpText("model <- lm(Y ~ X, data=mydata)"),
+               br(),
+               helpText("#Summary statistics for the model"),
+               helpText("summary(model)"),
+               br(),
+               helpText("#Plot the data and add the regression line"),
+               helpText("plot(Y ~ X, data=mydata)"),
+               helpText("abline(model)")
+      )
+    )
+  )
+  
+  ))
---
+++ server.R
@@ -0,0 +1,47 @@
+library(shiny)
+library(plotrix)
+
+shinyServer(function(input, output){
+  
+  data <- reactive({
+    xx <- seq(from=0,to=100,length.out = 30*input$n)
+    set.seed(100)
+    slope <- input$slope
+    if(slope == "Flat") slp = 0
+    if(slope == "Slight") slp = 0.5   
+    if(slope == "Steeper") slp = 1
+    if(slope == "Steepest") slp = 2.5
+    noise <- input$noise
+    if(noise == "Low") sdx = 25
+    if(noise == "Medium") sdx = 75
+    if(noise == "High") sdx = 200
+    yy <- slp*xx + rnorm(30*input$n, 0, sd=sdx) + 100
+    data.frame(X=xx, Y=yy)
+  })
+  
+  output$plot <- renderPlot({
+    plot(data(), xlab="X", ylab="Y", ylim=c(-300,800))
+    if(input$line) {
+      abline(lm(Y ~ X, data=data()), col="dark blue")
+    }
+    if(input$means) {
+      abline(v = mean(data()[,1]), lty="dotted")
+      abline(h = mean(data()[,2]), lty="dotted")
+    } 
+    if(input$ant) {
+      model = lm(Y ~ X, data=data())
+      txt = paste("The equation of the line is:\nY = ",
+                  round(coefficients(model)[1],0)," + ",
+                  round(coefficients(model)[2],3),"X + error")
+      
+      boxed.labels(50,600,labels=txt,bg="white", cex=1.25)
+    }    
+    
+  })
+  
+  output$summary <- renderPrint({
+    model = lm(Y ~ X, data=data())
+    summary(model)
+  })
+  
+})
